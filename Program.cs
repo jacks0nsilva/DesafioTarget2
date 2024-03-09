bc224604316b5652ce76e86f54bc7f89a3e03e2b
J4CKS0NBR <jacksonsilva_fsa@outlook.com>
2024-03-09 15:33:03 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using DesafioTarget2;
+
+bool executarNovamente = true;
+
+while (executarNovamente)
+{
+
+    Console.Write("Digite um número para verificar se pertence à sequência de Fibonacci: ");
+    int numero = Convert.ToInt32(Console.ReadLine());
+
+    bool pertence = VerificarSequenciaFibonacci.VerificarSequencia(numero);
+
+    if (pertence)
+    {
+        Console.WriteLine($"{numero} pertence à sequência de Fibonacci.");
+    }
+    else
+    {
+        Console.WriteLine($"{numero} não pertence à sequência de Fibonacci.");
+    }
+
+    Console.Write("\nDeseja verificar outro número? (S/N)   ");
+
+    char resposta = char.ToUpper(Console.ReadKey().KeyChar);
+    Console.WriteLine();
+    executarNovamente = (resposta == 'S');
+}
---
+++ VerificarSequenciaFibonacci.cs
@@ -0,0 +1,32 @@
+﻿namespace DesafioTarget2;
+
+public static class VerificarSequenciaFibonacci
+{
+    // Verifica se um número pertence à sequência de Fibonacci
+    public static bool VerificarSequencia(int numero)
+    {
+        
+        int anterior = 0;
+        int atual = 1;
+
+        // Caso especial para o zero
+        if (numero == 0)
+        {
+            return true;
+        }
+
+        while (atual <= numero)
+        {
+            if (atual == numero)
+            {
+                return true;
+            }
+
+            int proximo = anterior + atual;
+            anterior = atual;
+            atual = proximo;
+        }
+
+        return false;
+    }
+}
